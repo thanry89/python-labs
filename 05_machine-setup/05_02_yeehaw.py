ad139a5113c4431a57ca9e1aa15164d11e6e9e73
JonathanZ <thanry89@gmail.com>
2024-06-13 19:28:57 -0500

    Finished Lesson 5

--- 05_machine-setup/05_02_yeehaw.py
+++ 05_machine-setup/05_02_yeehaw.py
@@ -3,3 +3,4 @@
 # 	I'm a programmer now.
 # 	Yeehaw!
 # 	Coding here I come!
+print("I'm a programmer now.\nYeehaw\nCoding here I come!")
--- 05_machine-setup/05_04_seconds_year.py
+++ 05_machine-setup/05_04_seconds_year.py
@@ -1,2 +1,4 @@
 # Use the interpreter to calculate how many seconds are in a year.
 # Then write the code in this script file below this line.
+calc = 365*24*60
+print('A year has',calc,'seconds' )
